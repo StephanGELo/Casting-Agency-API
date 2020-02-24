4915ef9d4164bd507093bce54ac2dccbef5eb00f
Stephan Lefort <lefortsge@gmail.com>
2020-02-24 02:36:54 -0800

    feat: Implement get all movies endpoint

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,11 @@ from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 import json
 
-from backend.models import setup_db, Movie, Actor
+from models import setup_db, Movie, Actor
 
 ITEMS_PER_PAGE = 6
 
-def paginate_items = (request, list):
+def paginate_items(request, list):
     page = request.args.get('page', 1, type=int)
     start = (page - 1) * ITEMS_PER_PAGE
     end = start + ITEMS_PER_PAGE
@@ -22,9 +22,37 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+    CORS(app, resources={r"/api/": {"origins": "*"}})
 
-    @app.route('/')
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Authorization, true'
+        )
+        response.headers.add(
+            'Access-Control-Allow-Methods',
+            'GET, POST, DELETE, PATCH'
+        )
+        return response
+
+
+    #-------------------------------------------------------#
+    # ROUTES
+    #-------------------------------------------------------#
+
+    #--------------------#
+    # Movies
+    #--------------------#
+
+    @app.route('/movies', methods=['GET'])
     def get_movies():
-        return "https://youtu.be/kNw8V_Fkw28"
+        movies = Movie.query.all()
+        formatted_movies = [movie.format() for movie in movies]
+
+        return jsonify({
+            "success": "True",
+            "movies": formatted_movies
+        })
         
     return app
