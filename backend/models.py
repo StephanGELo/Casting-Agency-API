7aa21715469d4d8f95182603237c859231d8f283
Stephan Lefort <lefortsge@gmail.com>
2020-02-23 23:01:28 -0800

    chore: Create models for movies and actors

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,10 @@
 import os
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import json
 
-database_path = os.environ['DATABASE_URL']
+database_path = os.environ.['DATABASE_URL']
 
 db = SQLAlchemy()
 
@@ -19,14 +20,79 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
-'''
-Movie
-Have title and release year
-'''
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+#------------------------#
+# Movie model
+#------------------------#
 
 class Movie(db.Model):
     __tablename__ = 'Movies'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    release_year = db.Column(db.String(4), nullable=False)
+    release_date = db.Column(db.String(120), nullable=True)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+
+#------------------------#
+# Actor model
+#------------------------#
+
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def insert(self):
+        db.session.insert(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.update(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
