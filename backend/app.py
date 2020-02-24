7daa075b57f168ce92d0431eda3d8093f014b790
Stephan Lefort <lefortsge@gmail.com>
2020-02-24 01:06:33 -0800

    feat: Add pagination to list

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
-from backend.models import setup_db
+import json
+
+from backend.models import setup_db, Movie, Actor
+
+ITEMS_PER_PAGE = 6
+
+def paginate_items = (request, list):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+
+    formatted_items = [item.format() for item in list]
+    paginated_items = formatted_items[start:end]
+
+    return paginated_items
 
 def create_app(test_config=None):
 
