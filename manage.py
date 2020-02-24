14aacf2ea66ba4029bdc5bff0ba6387d783087b2
Stephan Lefort <lefortsge@gmail.com>
2020-02-23 23:27:31 -0800

    fix: Correct configuration settings

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from backend.app import create_app
-from models import db
+from backend.models import db
 
 app = create_app()
 migrate = Migrate(app, db)
