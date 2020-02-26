7da93e7e0491b9ac0fc6d7dba7bdaebb1acb18ce
Stephan Lefort <lefortsge@gmail.com>
2020-02-25 17:23:34 -0800

    chore: Implement code to verify the token.

--- backend/auth.py
+++ backend/auth.py
@@ -43,6 +43,8 @@ def get_token_auth_header():
 
     token = header_parts[1]
     return token
+
+
 #----------------------------------------------------------#
 # Check Permissions
 #----------------------------------------------------------#
@@ -59,3 +61,61 @@ def check_permissions(permission, payload):
         }, 403)
 
     return True
+
+
+#----------------------------------------------------------#
+# Verify the Token
+#----------------------------------------------------------#
+def verify_decode_jwt(token):
+    # Get the public key from Auth0
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+
+    # Get the data in the header
+    raw_header = jwt.get_unverified_header(token)
+
+    # Choose our key
+    rsa_key = {}
+    if 'kid' not in raw_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] = raw_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            # Use the key to validate the jwt
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+            return payload
+        
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+        
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims.\
+                    Please, check the audience and issuer.'
+            }, 400)
+    raise AuthError({
+        'code': 'invalid_header',
+        'description': 'Unable to find the appropriate key'
+    }, 401)
