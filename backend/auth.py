a0acac5ec31d2d00d1fcd80430e8aab1208f0f57
Stephan Lefort <lefortsge@gmail.com>
2020-02-25 17:22:21 -0800

    chore: Implement code the check Auth Permissions

--- backend/auth.py
+++ backend/auth.py
@@ -43,3 +43,19 @@ def get_token_auth_header():
 
     token = header_parts[1]
     return token
+#----------------------------------------------------------#
+# Check Permissions
+#----------------------------------------------------------#
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permission not included in JWT.'
+        }, 400)
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
+
+    return True
