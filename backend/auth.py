d6b757f753846eb6b1af7e5b9fdf8c5a6186502c
Stephan Lefort <lefortsge@gmail.com>
2020-02-25 17:21:13 -0800

    chore: Implement code for Auth Header

--- backend/auth.py
+++ backend/auth.py
@@ -8,7 +8,10 @@ AUTH0_DOMAIN = 'universaleagle.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'castingagency'
 
+
+#----------------------------------------------------------#
 # AuthError Exception
+#----------------------------------------------------------#
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
@@ -18,3 +21,25 @@ class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
+
+
+#----------------------------------------------------------#
+# Auth Header
+#----------------------------------------------------------#
+
+def get_token_auth_header():
+    authorization_header = request.headers.get('Authorization', None)
+    if not authorization_header:
+        raise AuthError({
+            'code': 'missing_header',
+            'description': 'The header must contain a "Bearer".'
+        }, 401)
+    
+    elif len(header_parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'The header must contain a bearer and the token.'
+        }, 401)
+
+    token = header_parts[1]
+    return token
