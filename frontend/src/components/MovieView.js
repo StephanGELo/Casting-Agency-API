ce8360c7fd7522507ad68d1d15547214300c3844
Stephan Lefort <lefortsge@gmail.com>
2020-03-18 17:45:52 -0700

    chore:change format of MovieView

--- frontend/src/components/MovieView.js
+++ frontend/src/components/MovieView.js
@@ -1,80 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Button } from 'reactstrap';
-import { Navlink as RouteNavlink, Route, Switch } from 'react-router-dom';
-// import jwt from 'jwt-decode';
-import axios from 'axios';
-import { useAuth0 } from '../react-auth0-spa';
-import { Movie } from './Movie';
-import config from '../auth_config.json';
+import React, { useState, useEffect } from "react";
+import { Container, Row, Button } from "reactstrap";
+import { NavLink as RouterNavLink, Route, Switch } from "react-router-dom";
+import jwt from "jwt-decode";
+import { useDataFetching } from "../hooks/useDataFetch";
+import { useAuth0 } from "../react-auth0-spa";
+import { Movie } from "./Movie";
+import { API_URL } from "../utils/auth_config";
+import { Loader } from "./Loader";
 
-// // const URL = 'https://stephangelcasting.herokuapp.com/movies';
-// const URL = 'http://localhost:5000/movies';
-// // const DEFAULT_QUERY = '1';
+const Movies = () => {
+    const [response, setResponse] = useState({});
+    const [pageNum, setPageNum] = useState(1);
+    const [token, setToken] = useState();
+    const { getTokenSilently, user, loading } = useAuth0();
 
-// const Movies = () => {
-//    const [result, setResult] = useState({});
-//    const [token, setToken] = useState();
-//    const { getTokenSilently, user, loading } = useAuth0();
+    const url = `${API_URL}/movies?page=${pageNum}`;
+    console.log("url is: ", url)
+    const result = useDataFetching(url, {}, token) || {};
 
-//     return (
-//         <div>
-//             <h1>
-//                 Movies!
-//             </ h1>
-//             <p>
-//                     This is a list of movies to be released soon.
-//             </p>
-//             <Row>
-//                 {
-//                     result.movies.map(movie => (
-//                         <Movie 
-//                             key={movie.id}
-//                             movie={movie}
-//                             token={token}
-//                         />
-//                     ))
-//                 }
-//             </Row>
-//         </div>
-//     );
-// };
+    useEffect(() => {
+        setResponse(result)
+    }, [result])
 
-// export const RouteMovies = () => {
-//     return (
-//         <Switch>
-//             <Route path="/movies" component={Movies} />
-//         </Switch>
-//     )
-// }
+    if (user && !loading) getTokenSilently().then(res => setToken(res));
 
-// import React from 'react';
-// import axios from 'axios';
+    let decodedToken;
+    if (token) decodedToken = jwt(token);
 
-export default class Movies extends React.Component {
-    state = {
-        movies : []
-    }
+    const selectPage = num => setPageNum(num);
+    const create_pagination = () => {
+        let pageNumbers = [];
+        let maxPage = Math.ceil(result.total_movies / 10);
 
-    componentDidMount() {
-        axios.get('https://stephangelcasting.herokuapp.com/movies')
-            .then(res => {
-                const movies = res.data.movies;
-                this.setState({ movies });
-            })
-    }
+        for (let i = 1; i <= maxPage; i++) {
+            pageNumbers = [
+                ...pageNumbers,
+                <span
+                    key={i}
+                    className={`page-num ${i === pageNum ? "active" : ""}`}
+                    onClick={() => {
+                        selectPage(i);
+                    }}
+                >
+                    {i}
+                </span>
+            ];
+        }
+        return pageNumbers;
+    };
 
-    render() {
-        return (
-            <ul>
-                { this.state.movies.map(movie => <li>{movie.title} {movie.release_date}</li>)}
-            </ul>
-        )
-    }
-}
+    const deleteMovie = async id => {
+        const result = await fetch(`${API_URL}/movies/${id}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: "Bearer " + token,
+                "Content-Type": "application/json"
+            }
+        });
+        const getResult = await result.json()
+        setResponse(getResult)
+    };
+
+    return (
+        <>
+            <Container>
+                <h1>Recent Movies</h1>
+                {decodedToken &&
+                    decodedToken.permissions.indexOf("post:movies") !== -1 ? (
+                        <Button color="primary" to={{
+                            pathname: "/movies/add-movie",
+                            state: { editing: false, movieData: null, token: token }
+                        }} tag={RouterNavLink}>
+                            Add a movie
+                        </Button>
+                    ) : null}
+                <Row>
+                    {response.movies ? (
+                        response.movies.map(movie => (
+                            <Movie
+                                key={movie.id}
+                                movie={movie}
+                                exposedToken={decodedToken}
+                                token={token}
+                                deleteMovie={deleteMovie}
+                            />
+                        ))
+                    ) : (
+                            <Loader />
+                        )}
+                </Row>
+                <Row className="justify-content-center">{create_pagination()}</Row>
+            </Container>
+        </>
+    );
+};
 
 export const RouteMovies = () => {
     return (
         <Switch>
+            {/* <Route path="/movies/add-movie" component={AddMovie} /> */}
             <Route path="/movies" component={Movies} />
         </Switch>
     )
