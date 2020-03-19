325d5a43184ebeed6eb2a53b3e2a91a490850497
Stephan Lefort <lefortsge@gmail.com>
2020-03-18 17:57:11 -0700

    chore: Add configuration for fetching data

---
+++ frontend/src/utils/auth_config.js
@@ -0,0 +1,4 @@
+export const AUTH0_DOMAIN=process.env.AUTH0_DOMAIN;
+export const CLIENT_ID=process.env.CLIENT_ID;
+export const API_AUDIENCE=process.env.API_AUDIENCE;
+export const API_URL="https://stephangelcasting.herokuapp.com"
