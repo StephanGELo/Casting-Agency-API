6075aa2a794ed4df2f2be447f1c122ad9bb82db4
Stephan Lefort <lefortsge@gmail.com>
2020-03-18 17:52:24 -0700

    chore: Add hooks for data fetching

---
+++ frontend/src/hooks/useDataFetch.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+export const useDataFetching = (url, initialData, token, body) => {
+    const [data, setData] = useState(initialData)
+
+    useEffect(() => {
+        async function retrieveData() {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    Authorization: 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+            setData(await response.json())
+        }
+        retrieveData()
+    }, [url, token])
+    
+    return data;
+}
