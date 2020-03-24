b35949c7ebde2b97d3a9653b09362929c27d9de6
Stephan Lefort <lefortsge@gmail.com>
2020-03-24 16:09:46 -0700

    chore: Add path to Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,36 @@
 import React from 'react';
 // import logo from './logo.svg';
-import NavBar from "./components/NavBar";
+import { NavBar } from "./components/NavBar";
 import { useAuth0 } from "./react-auth0-spa";
 import { Router, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
 import history from "./utils/history";
 import PrivateRoute from "./components/PrivateRoute";
 import { RouteMovies } from './components/MovieView';
+import { Home } from './components/Home';
+import { Container } from "reactstrap";
 // import { RouteActors } from './components/Actors';
 import './App.css';
 
 function App() {
-  const { loading } = useAuth0();
+  const { loading, user } = useAuth0();
 
-  if (loading) {
-    return <div>Loading...</div>;
+  if (loading && !user) {
+    return <p>Loading...</p>
   }
-
   return (
-    <div className="App">
-      <Router history={history}>
-        <header>
-          <NavBar />
-        </header>
-        <Switch>
-          <Route path="/" exact/>
-          <Route path='/movies' component={RouteMovies} />
-          {/* <Route path='/actors' component={RouteActors} /> */} 
-          {/* <PrivateRoute path="/profile" component={Profile} /> */}
-        </Switch>
-      </Router>
-    </div>
+    <Router history={history}>
+      <div id="app" className="d-flex flex-column w-100">
+        <NavBar />
+        <Container className="flex-grow-1 mt-5">
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/movies' component={RouteMovies} />
+            {/* <Route path='/actors' component={RouteActors} /> */}
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
