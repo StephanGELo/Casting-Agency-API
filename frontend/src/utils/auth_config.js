9704f6616faac827705bd0220d065c80585005fd
Stephan Lefort <lefortsge@gmail.com>
2020-03-24 17:35:50 -0700

    chore: delete unwanted exports

--- frontend/src/utils/auth_config.js
+++ frontend/src/utils/auth_config.js
@@ -1,4 +1,1 @@
-export const AUTH0_DOMAIN=process.env.AUTH0_DOMAIN;
-export const CLIENT_ID=process.env.CLIENT_ID;
-export const API_AUDIENCE=process.env.API_AUDIENCE;
-export const API_URL="https://stephangelcasting.herokuapp.com"
+export const API_URL="https://stephangelcasting.herokuapp.com/movies"
