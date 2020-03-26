6876533cde8e812f987e3fe65d2f2c4bc80d5e8f
Stephan Lefort <lefortsge@gmail.com>
2020-03-26 15:57:51 -0700

    chore: Create form skeleton to add a new movie

--- frontend/src/components/Forms/AddMovie.js
+++ frontend/src/components/Forms/AddMovie.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { 
     Card,
-    CardHeader
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    FormText
  } from 'reactstrap';
 
 export const AddMovie = () => {
     return (
-        <Card>
+        <Card inverse style={{ backgroundColor:'#333', borderColor:'#333'}}>
             <CardHeader>Add a new Movie</CardHeader>
+            <CardBody>
+                <Form>
+                    <FormGroup>
+                        <Label for="movieTitle">Movie Title</Label>
+                        <Input type="textarea" name="movieTitle" id="movieTitle" />
+                        <FormText>Enter the title of the movie.</FormText>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="movieReleaseDate">Movie Release Date</Label>
+                        <Input type="date" name="movieReleaseDate" id="movieReleaseDate" />
+                        <FormText>Enter the release date of the movie.</FormText>
+                    </FormGroup>
+                </Form>
+            </CardBody>
+            <CardFooter>
+                <Button className="btn btn-sm">Submit</Button>
+            </CardFooter>
         </Card>
     )
 };
