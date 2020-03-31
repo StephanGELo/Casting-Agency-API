55a50ac1e66ff13477e4da166ab117519d18ee3b
Stephan Lefort <lefortsge@gmail.com>
2020-03-30 21:45:36 -0700

    chore: Add button to add new actor

--- frontend/src/components/ActorView.js
+++ frontend/src/components/ActorView.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Button } from 'reactstrap';
 import { Route, Switch } from 'react-router-dom';
+import { Actor } from "./Actor";
 
 const Actors = () => {
     return (
         <>
-            <div>
+            <Container>
                 <h1>Recent Actors</h1>
-            </div>
+                <Button color='primary' style="text-right">Add a New Actor</Button>
+                <Row>
+                    <Actor />
+                </Row>
+            </Container>
         </>
     );
 };
