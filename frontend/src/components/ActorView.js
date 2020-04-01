15a95471648fcbfc2b93d2a1ad0e93a878e55284
Stephan Lefort <lefortsge@gmail.com>
2020-03-31 17:16:52 -0700

    chore: Fetch data from backend api

--- frontend/src/components/ActorView.js
+++ frontend/src/components/ActorView.js
@@ -1,15 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Button } from 'reactstrap';
-import { Route, Switch } from 'react-router-dom';
+import { NavLink as RouterNavLink, Route, Switch } from 'react-router-dom';
 import { Actor } from "./Actor";
+import { useAuth0 } from "../react-auth0-spa";
+import { useDataFetching } from "../hooks/useDataFetch";
+import { Loader } from "./Loader";
+import { API_URL } from "../utils/auth_config";
+import * as JWT from 'jwt-decode';
+
 
 const Actors = () => {
+    const [response, setResponse] = useState({});
+    const [token, setToken] = useState();
+    const { getTokenSilently, user, loading } = useAuth0();
+
+    const url = `${API_URL}/actors`;
+
+    const result = useDataFetching(url, {}, token) || {};
+
+    useEffect(() => {
+        setResponse(result)
+    }, [result])
+
+    if (user && !loading) getTokenSilently().then(res => setToken(res));
+
+    let decodedToken = "";
+
+    try {
+        if (token) {
+            decodedToken = JWT(token)
+        }
+    } catch(error) {
+        console.log(error)
+    }
+
+
     return (
         <>
             <Container>
                 <h1>Recent Actors</h1>
-                <Button color='primary' style="text-right">Add a New Actor</Button>
+                {decodedToken && decodedToken.permissions.indexOf("post:actors") !== -1 ? (
+                    <div className="text-right">
+                        <Button color="primary" to={{
+                            pathname:"/actors/AddNewActor",
+                            state: { editing: false, movie:null, token: token }
+                        }} tag={RouterNavLink}>
+                        Add a New Actor
+                        </Button>
+                    </div>
+                ): null}
                 <Row>
+                    {response.actors ? (
+                        response.actors.map(actor => (
+                            <Actor 
+                                key={actor.id}
+                                actor={actor}
+                                exposedToken={decodedToken}
+                                token={token}
+                            />
+                        ))
+                        ) : (
+                                <Loader />
+                            )
+                    }
                     <Actor />
                 </Row>
             </Container>
