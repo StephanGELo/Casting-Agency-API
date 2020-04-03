1aa9f20107229790a5817a85f4b0d28a3218b62b
Stephan Lefort <lefortsge@gmail.com>
2020-04-03 16:03:20 -0700

    chore: Render all form fields for addition of actor

--- frontend/src/components/Forms/AddActorForm.js
+++ frontend/src/components/Forms/AddActorForm.js
@@ -32,11 +32,53 @@ export const AddActorForm = props => {
         })
     };
 
+
     return (
         <Card inverse style={{ backgroundColor:'#333', borderColor:'#333'}}>
             <CardHeader>Add a new Actor</CardHeader>
             <CardBody>
-
+                <Form>
+                    <FormGroup>
+                        <Label for="actorName">Actor's Name'</Label>
+                        <Input 
+                            type="text"
+                            name="actorName"
+                            id="actorName"
+                            onChange={e => updateFormFields("name", e.target.value)}
+                        />
+                        <FormText>Enter the name of the actor.</FormText>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="actorAge">Actor's age</Label>
+                        <Input 
+                            type="text"
+                            name="actorAge"
+                            id="actorAge"
+                            onChange={e => updateFormFields("age", e.target.value)}
+                        />
+                        <FormText>Enter the age of the actor.</FormText>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="actorGender">Actor's gender</Label>
+                        <Input 
+                            type="text"
+                            name="actorGender"
+                            id="actorGender"
+                            onChange={e => updateFormFields("gender", e.target.value)}
+                        />
+                        <FormText>Enter the gender of the actor.</FormText>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="assignedMovie">Assigned to Movie</Label>
+                        <Input 
+                            type="text"
+                            name="assignedMovie"
+                            id="actorMovie"
+                           onChange={e => updateFormFields("movie", e.target.value)}
+                        />
+                        <FormText>Enter the title of the Movie assigned to the actor.</FormText>
+                    </FormGroup>
+                </Form>
             </CardBody>
             <CardFooter className="d-flex justify-content-between">
                 <Button tag={RouterNavLink} to="/actors-details" color="warning">Cancel</Button>
